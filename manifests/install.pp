917923f2a90bede9ee791fe505c08a19a171642d
Gregory J. Knight <gknight.dev@gmail.com>
2016-02-04 08:15:11 -0500

    add mod_wsgi_package

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,6 +7,10 @@ class panopuppet::install {
 
   } ->
 
+  package { $::panopuppet::python3_modwsgi_package :
+    ensure => $::panopuppet::python3_modwsgi_package_ensure,
+  } ->
+  
   package { $::panopuppet::panopuppet_package :
     ensure => $::panopuppet::python3_modwsgi_package_ensure,
   }
